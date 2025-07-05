e3ebbb14a52e55c2701af4d9d09a21f155f4aeb9
Asif Sayyed <asifdotexe@gmail.com>
2025-07-05 13:07:04 +0530

    #1 create a test script

---
+++ code/test_scripts/check_number_sign.py
@@ -0,0 +1,18 @@
+"""
+This is a simple function to rest the application
+"""
+
+def check_number_sign(number: float) -> None:
+    """This function uses if-else to check the arithmetic sign that number has.
+
+    :param number: Should be a non-string and non-null value
+    """
+    if number > 0:
+        print("The number is positive.")
+    else:
+        print("The number is zero or negative.")
+
+# Test cases
+check_number_sign(5)
+check_number_sign(-3)
+check_number_sign(0)
