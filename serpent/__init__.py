5f8fee4dca4b74ffbbc3f2a66d6448efc041e1db
Asif Sayyed <asifdotexe@gmail.com>
2025-07-20 13:43:15 +0530

    #14 renamed the dir and removed test

---
+++ serpent/__init__.py
@@ -0,0 +1,5 @@
+"""
+Contains the main entry point for the SERPENT application.
+
+This module sets up the Streamlit web application for converting Python functions into flowcharts using Graphviz
+"""
