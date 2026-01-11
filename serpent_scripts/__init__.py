a09b2162564e82a5fe8836b8c15fe903ab8c7bfd
Asif Sayyed <asifdotexe@gmail.com>
2026-01-11 12:32:28 +0530

    #27 deleted legacy serpent script

--- serpent_scripts/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Contains the main entry point for the SERPENT application.
-
-This module sets up the Streamlit web application for converting Python functions into flowcharts using Graphviz
-"""
